f9ff8f0c51fe82775b6aab78faf31d17d7ee0b87
zhangyz <zhangyuzeoffice@163.com>
2018-04-13 09:59:20 +0800

    commit

--- scripts/生成ip地址.py
+++ scripts/生成ip地址.py
@@ -1,11 +1,11 @@
 def get_ip():
-	import random
+    import random
 
-	l = []
-	for i in range(0,4):
-	n = random.randint(1,255)
-	l.append(str(n))
-	return ".".join(l)
+    l = []
+    for i in range(0,4):
+        n = random.randint(1,255)
+        l.append(str(n))
+    return ".".join(l)
 
-	ip = get_ip()
+ip = get_ip()
 print(ip)
