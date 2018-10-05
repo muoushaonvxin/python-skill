5ad67f49eae296c81ed48fc8acda5b6b4e0ca921
seliencepuppet <zhangyuzeoffice@163.com>
2018-10-05 21:14:50 +0800

    Update application.py

--- tornado/framework/example/application.py
+++ tornado/framework/example/application.py
@@ -7,7 +7,7 @@ class Application(tornado.web.Application):
     def __init__(self):
 	handlers = [
 	    (r'/', index.IndexHandler),
-	    (r'/sunck', index.SunckHandler),
+	    (r'/home', index.HomeHandler),
 	    (r'(.*)$', tornado.web.StaticFileHandler, {"path": os.path.join(config.BASE_DIRS, "static/html"), 
                                                        "default_filename": "index.html"})
 	]
