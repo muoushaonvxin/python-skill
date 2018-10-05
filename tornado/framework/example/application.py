7d84a77041c2bed17a321e19d1136c7c28ead986
seliencepuppet <zhangyuzeoffice@163.com>
2018-10-05 21:07:04 +0800

    Update application.py

--- tornado/framework/example/application.py
+++ tornado/framework/example/application.py
@@ -1,11 +1,14 @@
 import tornado.web
 from views import index
 import config
+import os
 
 class Application(tornado.web.Application):
-	def __init__(self):
-		handlers = [
-			(r'/', index.IndexHandler),
-			(r'/sunck', index.SunckHandler)
-		]
-		super(Application, self).__init__(handlers, **config.settings)
+    def __init__(self):
+	handlers = [
+	    (r'/', index.IndexHandler),
+	    (r'/sunck', index.SunckHandler),
+	    (r'(.*)$', tornado.web.StaticFileHandler, {"path": os.path.join(config.BASE_DIRS, "static/html"), 
+                                                       "default_filename": "index.html"})
+	]
+	super(Application, self).__init__(handlers, **config.settings)
